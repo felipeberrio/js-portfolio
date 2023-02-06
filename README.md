4465a845f09b92d0da4d02df6f17024e2972529b
felipeberrio <cristianberrio95@gmail.com>
2023-02-06 04:48:09 -0500

    Configuramos los plugin de webpack

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # js-portfolio
+
+1. Iniciamos la confiugración webpack con: npx webpack --mode development --config webpack.config.js
+    Para hacer más amigable la ejecución del comando podemos crear un script de inicio en nuestro package.json build
+    Lo utilizamos con: npm run build
+
+2. Para dejar nuestro proyecto preparado para los diferentes navegadores que existen podemos utilizar babel loader en javascript
+    instalamos las depencias como desarrollo con: npm install -D babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime
+    Creamos el archivo de configuración de babel: .babelrc
+    Añadimos la confiugración de babel a webpack.config.js
+        Volvemos a correr el archivo con: npm run build
+
+3. Instalamos el plugin de html en webpack para utilizarlo con: 
+    npm i html-webpack-plugin -D y agregamos el plugin dentro de nuestra configuración webpack
+
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path'); //Requerimos path que ya esta instalada
+const HtmlWebpackPlugin = require('html-webpack-plugin') //Requerimos el plugin instalado de html
+
+module.exports = { // añadimos los módulos
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'), // traemos el path que traemos al comienzo para usar resolve, que sirve para que localice la carpeta dist de output
+        filename: 'main.js',
+    },
+    resolve: {
+        extensions: ['.js'],// Agregamos que tipo de extenciones manejara webpack IMPORTANTE
+    },
+    module: {
+        rules: [
+          {
+            // Test declara que extensión de archivos aplicara el loader
+            test: /\.js$/,
+            // Use es un arreglo u objeto donde dices que loader aplicaras
+            use: {
+              loader: "babel-loader"
+            },
+            // Exclude permite omitir archivos o carpetas especificas
+            exclude: /node_modules/
+          }
+        ]
+      },
+      // SECCION DE PLUGINS
+    plugins: [
+        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
+            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+            filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
+        })
+    ]
+}
